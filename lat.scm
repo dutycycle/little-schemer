9312274c889d053e4c2714c4a7f569523c4e3cc2
Kai Rikhye <krikhye@gmail.com>
2014-10-01 22:35:01 -0400

    couple of more exercises

--- lat.scm
+++ lat.scm
@@ -22,3 +22,20 @@
      ((null? lat) ())
      ((eq? a (car lat)) (cdr lat))
      (else (cons (car lat) (rember a (cdr lat)))))))
+
+;; return a list of all the first s-expressions in a list of lists _l_
+
+(define firsts
+  (lambda (l)
+  (cond
+   ((null? l) ())
+   (else (cons (car (car l)) (firsts (cdr l)))))))
+
+;; insert _new_ to the right of _old_ in list of atoms _lat_
+
+(define insertR
+  (lambda (new old lat)
+    (cond
+     ((null? lat) ())
+     ((eq? old (car lat)) (cons (cons (car lat) (cons new ()) (cdr lat)))
+     (else (cons (car lat) (insertR new old (cdr lat))))))))
